16f22d5fc34fa34eb785ae40c9ace41feef6c3c6
syf <2654525303@qq.com>
2025-05-02 17:02:16 +0800

    create jigsaw board with padding

---
+++ src/Sandbox/Scripts/Jigsaw/JigsawBoard.cs
@@ -0,0 +1,60 @@
+﻿using Godot;
+using GodotUtilities;
+
+namespace Sandbox.Jigsaw;
+
+[Scene]
+public partial class JigsawBoard : Node2D
+{
+    private const string BackgroundImagePath = "res://Art/assets_to_dowmload/flower_12_8.jpg";
+
+    [Node]
+    private Sprite2D boardSprite = null!;
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+
+    public override void _Ready()
+    {
+        var texture = LoadBoardImageWithBorder(BackgroundImagePath, Vector2I.One * 10);
+        boardSprite.Texture = texture;
+    }
+
+
+    private Texture2D LoadBoardImageWithBorder(string imagePath, Vector2I borderSize)
+    {
+        var originalImage = GD.Load<Image>(imagePath);
+        var paddedImage = GetImageWithBorder(originalImage, borderSize);
+
+        var tex = new ImageTexture();
+        tex.SetImage(paddedImage);
+        return tex;
+    }
+
+    private Image GetImageWithBorder(Image contentImage, Vector2I borderSize)
+    {
+        var (borderX, borderY) = borderSize;
+        var (width, height) = contentImage.GetSize();
+        var paddedWidth = width + borderX * 2;
+        var paddedHeight = height + borderY * 2;
+        var paddedImage = Image.CreateEmpty(paddedWidth, paddedHeight, false, Image.Format.Rgba8);
+
+        paddedImage.Fill(Colors.White);
+
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                var color = contentImage.GetPixel(x, y);
+                paddedImage.SetPixel(x + borderX, y + borderY, color);
+            }
+        }
+
+        return paddedImage;
+    }
+}
