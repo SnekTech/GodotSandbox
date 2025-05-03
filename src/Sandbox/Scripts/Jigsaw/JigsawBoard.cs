e8f99af001e1101fc809c05111ee7281439b90a3
syf <2654525303@qq.com>
2025-05-03 21:18:33 +0800

    ghost(transparency) board

--- src/Sandbox/Scripts/Jigsaw/JigsawBoard.cs
+++ src/Sandbox/Scripts/Jigsaw/JigsawBoard.cs
@@ -7,6 +7,7 @@ namespace Sandbox.Jigsaw;
 public partial class JigsawBoard : Node2D
 {
     private const string BackgroundImagePath = "res://Art/assets_to_dowmload/flower_12_8.jpg";
+    private const float GhostBoardTransparency = 0.1f;
 
     [Node]
     private Sprite2D board = null!;
@@ -27,6 +28,8 @@ public partial class JigsawBoard : Node2D
         var boardImage = GD.Load<Image>(BackgroundImagePath);
         InitBorderBackground(boardImage, Vector2I.One * 10, Colors.White);
         LoadBoardTexture(boardImage);
+
+        ShowGhostBoard();
     }
 
     private void LoadBoardTexture(Image boardImage)
@@ -45,9 +48,24 @@ public partial class JigsawBoard : Node2D
                 Image.Format.Rgba8);
         backgroundBorderImage.Fill(color);
 
+        for (var x = borderWidth; x < borderWidth + contentWidth; x++)
+        {
+            for (var y = borderHeight; y < borderHeight + contentHeight; y++)
+            {
+                backgroundBorderImage.SetPixel(x, y, Colors.Transparent);
+            }
+        }
+
         var backgroundBorderTexture = new ImageTexture();
         backgroundBorderTexture.SetImage(backgroundBorderImage);
         border.Texture = backgroundBorderTexture;
         border.Offset = new Vector2(-borderWidth, -borderHeight);
     }
+
+    private void ShowGhostBoard()
+    {
+        SetBoardAlpha(GhostBoardTransparency);
+    }
+
+    private void SetBoardAlpha(float alpha) => board.Modulate = border.Modulate with { A = alpha };
 }
