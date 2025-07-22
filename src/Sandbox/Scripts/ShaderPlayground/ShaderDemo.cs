dbe02ab4d6ba2fa21b74c1d9c434d11e8d15eabd
syf <2654525303@qq.com>
2025-07-22 15:21:54 +0800

    Getting Started

--- src/Sandbox/Scripts/ShaderPlayground/ShaderDemo.cs
+++ src/Sandbox/Scripts/ShaderPlayground/ShaderDemo.cs
@@ -1,7 +1,36 @@
-﻿namespace Sandbox.ShaderPlayground;
+﻿using GodotGadgets.Extensions;
+
+namespace Sandbox.ShaderPlayground;
 
 [SceneTree]
 public partial class ShaderDemo : Node2D
 {
+    private static readonly StringName UniformMouse = "u_mouse";
+    private static readonly StringName UniformResolution = "u_resolution";
     
+    private Vector2 _picSize;
+    private ShaderMaterial _shader = null!;
+
+    public override void _Ready()
+    {
+        _picSize = Pic.Texture.GetSize() * Pic.Scale;
+        _shader = (ShaderMaterial)Pic.Material;
+    }
+
+    public override void _Process(double delta)
+    {
+        _shader.SetShaderParameter(UniformMouse, GetMousePositionInPic());
+        _shader.SetShaderParameter(UniformResolution, _picSize);
+    }
+
+    private Vector2 GetMousePositionInPic()
+    {
+        var (relativeX, relativeY) = GetGlobalMousePosition() - _Pic.GlobalPosition;
+        var (width, height) = _picSize;
+
+        relativeX = Mathf.Clamp(relativeX, 0, width) / width;
+        relativeY = Mathf.Clamp(relativeY, 0, height) / height;
+
+        return new Vector2(relativeX, relativeY);
+    }
 }
