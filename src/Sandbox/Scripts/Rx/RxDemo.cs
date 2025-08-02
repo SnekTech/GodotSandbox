8988f1f732d94ad2c49dc8da8f26dfa0688c456e
syf <2654525303@qq.com>
2025-08-02 21:56:12 +0800

    try R3

---
+++ src/Sandbox/Scripts/Rx/RxDemo.cs
@@ -0,0 +1,30 @@
+﻿using R3;
+
+namespace Sandbox.Rx;
+
+[SceneTree]
+public partial class RxDemo : Node2D
+{
+    private IDisposable? _subscription;
+
+    private readonly BehaviorSubject<int> countSubject = new(0);
+
+    private Observable<int> CountObservable => countSubject;
+
+    public override void _Ready()
+    {
+        AddButton.Pressed += () =>
+        {
+            if (countSubject.IsDisposed) return;
+
+            countSubject.OnNext(countSubject.Value + 1);
+        };
+
+        _subscription = CountObservable.Subscribe(count => CountLabel.Text = count.ToString());
+    }
+
+    public override void _ExitTree()
+    {
+        _subscription?.Dispose();
+    }
+}
