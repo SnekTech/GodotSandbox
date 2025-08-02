312a100fba9ffcf6ac78ab2afebe8c89d983287b
syf <2654525303@qq.com>
2025-08-02 18:51:51 +0800

    try custom drawing

---
+++ src/Sandbox/Scripts/CustomDrawing/CustomDrawingDemo.cs
@@ -0,0 +1,12 @@
+﻿namespace Sandbox.CustomDrawing;
+
+[SceneTree]
+public partial class CustomDrawingDemo : Node2D
+{
+    
+    public override void _Draw()
+    {
+        // DrawLine(Vector2.Zero, Vector2.One * 200, Colors.Aqua, 1);
+        DrawCircle(Vector2.Zero, 30, Colors.Aqua);
+    }
+}
