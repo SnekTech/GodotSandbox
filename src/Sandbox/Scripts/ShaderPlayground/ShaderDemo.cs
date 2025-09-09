6ffaca6458646ebe5cb7df8d9b86b425d4fa7713
syf <2654525303@qq.com>
2025-09-09 22:06:43 +0800

    shader demo cleanup

--- src/Sandbox/Scripts/ShaderPlayground/ShaderDemo.cs
+++ src/Sandbox/Scripts/ShaderPlayground/ShaderDemo.cs
@@ -5,15 +5,15 @@ namespace Sandbox.ShaderPlayground;
 [SceneTree]
 public partial class ShaderDemo : Node2D
 {
-    private Vector2 _picSize;
+    private Vector2 _canvasSize;
     private ShaderBookShader _shader = null!;
 
     public override void _Ready()
     {
-        _shader = new ShaderBookShader(Pic.GetMaterialAs<ShaderMaterial>());
+        _shader = new ShaderBookShader(Canvas.GetMaterialAs<ShaderMaterial>());
         
-        _picSize = Pic.Texture.GetSize() * Pic.Scale;
-        _shader.Resolution.Value = _picSize;
+        _canvasSize = Canvas.Texture.GetSize() * Canvas.Scale;
+        _shader.Resolution.Value = _canvasSize;
     }
 
     public override void _Process(double delta)
@@ -23,8 +23,8 @@ public partial class ShaderDemo : Node2D
 
     private Vector2 GetMousePositionInPic()
     {
-        var (relativeX, relativeY) = GetGlobalMousePosition() - _Pic.GlobalPosition;
-        var (width, height) = _picSize;
+        var (relativeX, relativeY) = GetGlobalMousePosition() - Canvas.GlobalPosition;
+        var (width, height) = _canvasSize;
 
         relativeX = Mathf.Clamp(relativeX, 0, width) / width;
         relativeY = Mathf.Clamp(relativeY, 0, height) / height;
