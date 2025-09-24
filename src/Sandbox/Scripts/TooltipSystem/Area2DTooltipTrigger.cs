1de47d7d907f1504181ddb2d2a9fb75177df5a5c
syf <2654525303@qq.com>
2025-09-24 10:07:56 +0800

    create abstract TooltipTrigger node

--- src/Sandbox/Scripts/TooltipSystem/Area2DTooltipTrigger.cs
+++ src/Sandbox/Scripts/TooltipSystem/Area2DTooltipTrigger.cs
@@ -1,18 +1,18 @@
 ﻿namespace Sandbox.TooltipSystem;
 
 [GlobalClass]
-public partial class ControlTooltipTrigger : TooltipTrigger
+public partial class Area2DTooltipTrigger : TooltipTrigger
 {
     public override void _EnterTree()
     {
-        var parent = GetParent<Control>();
+        var parent = GetParent<Area2D>();
         parent.MouseEntered += OnMouseEntered;
         parent.MouseExited += OnMouseExited;
     }
 
     public override void _ExitTree()
     {
-        var parent = GetParent<Control>();
+        var parent = GetParent<Area2D>();
         parent.MouseEntered -= OnMouseEntered;
         parent.MouseExited -= OnMouseExited;
     }
---
+++ src/Sandbox/Scripts/TooltipSystem/TooltipTrigger.cs
@@ -0,0 +1,14 @@
+﻿namespace Sandbox.TooltipSystem;
+
+public abstract partial class TooltipTrigger : Node
+{
+    protected static void OnMouseEntered()
+    {
+        TooltipLayer.ShowTooltip();
+    }
+
+    protected static void OnMouseExited()
+    {
+        TooltipLayer.HideTooltip();
+    }
+}
