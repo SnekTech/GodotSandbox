a2fd5fb9f96605802b03d5354ccf2f83b1060030
syf <2654525303@qq.com>
2025-12-27 22:07:35 +0800

    create a wrapper class library project for ms-tools native lib
    - explore native dll resolving mechanic
    - successfully export and execute

---
+++ src/Sandbox/Scripts/TestNativeLib/LayMineDemo.cs
@@ -0,0 +1,16 @@
+﻿using Dumpify;
+using MineSweeperTools;
+
+namespace Sandbox.TestNativeLib;
+
+[SceneTree]
+public partial class LayMineDemo : Node
+{
+    public override void _Ready()
+    {
+        var bombMatrix =
+            LayMineEngine.LayMineSolvable(16, 30, 99, 1, 1);
+
+        _.Label.Text = bombMatrix.DumpText();
+    }
+}
