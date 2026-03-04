b0ea33f6a47fee6713090a4bd3210123c49ab74f
syf <2654525303@qq.com>
2026-03-04 21:42:03 +0800

    uniform GTween extension

---
+++ src/Sandbox/Scripts/ShaderPlayground/UniformExtensions.cs
@@ -0,0 +1,34 @@
+﻿using GodotGadgets.ShaderStuff;
+using GTweens.Extensions;
+using GTweens.Tweeners;
+using GTweens.Tweens;
+using GTweensGodot.Extensions;
+
+namespace Sandbox.ShaderPlayground;
+
+public static class UniformExtensions
+{
+    extension<[MustBeVariant] T>(Uniform<T> uniform) where T : struct
+    {
+        Tweener<T>.Getter Getter => () => uniform.Value;
+        Tweener<T>.Setter Setter => v => uniform.Value = v;
+    }
+
+    extension(Uniform<float> uniform)
+    {
+        public GTween Tween(float to, float duration)
+            => GTweenExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
+    }
+
+    extension(Uniform<Vector2> uniform)
+    {
+        public GTween Tween(Vector2 to, float duration) =>
+            GTweenGodotExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
+    }
+
+    extension(Uniform<Color> uniform)
+    {
+        public GTween Tween(Color to, float duration) =>
+            GTweenGodotExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
+    }
+}
